a07679c0c704226e1866de050a1cdd7195096ea9
Kitae's Github <aqua0405@gmail.com>
2023-10-28 11:04:11 +0900

    Update MemberService.java

--- src/main/java/kr/inhatc/shop/service/MemberService.java
+++ src/main/java/kr/inhatc/shop/service/MemberService.java
@@ -3,6 +3,11 @@ package kr.inhatc.shop.service;
 import kr.inhatc.shop.entity.Member;
 import kr.inhatc.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,8 +15,9 @@ import java.util.Optional;
 
 @Service
 @Transactional
+@Slf4j
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -26,5 +32,27 @@ public class MemberService {
             System.out.println("member : " + findMember);
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    /**
+     * 로그인 시 사용자 정보를 DB에서 조회하여 반환하는 메소드
+     * @param email : 로그인 시 입력한 이메일
+     * @return : 사용자 정보
+     * @throws UsernameNotFoundException : 사용자 정보를 찾을 수 없을 경우 예외 발생
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : " + email));
+
+        log.info("login user : " + member);
+
+        // 스프링 시큐리티에서 제공하는 User 객체를 만들어 반환
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
