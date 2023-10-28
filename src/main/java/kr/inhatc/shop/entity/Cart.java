a3f3c87dbaa1719cf82c23db04359c5a74499f6f
Kitae's Github <aqua0405@gmail.com>
2023-10-28 11:04:03 +0900

    Update Cart.java

--- src/main/java/kr/inhatc/shop/entity/Cart.java
+++ src/main/java/kr/inhatc/shop/entity/Cart.java
@@ -18,8 +18,8 @@ public class Cart extends BaseEntity {
     @Column(name = "cart_id", nullable = false)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @OneToOne(fetch = FetchType.LAZY)       // 1:1 매핑, 지연로딩
+    @JoinColumn(name = "member_id")         // 외래키(FK)를 매핑할 때 사용
     private Member member;
 
 }
